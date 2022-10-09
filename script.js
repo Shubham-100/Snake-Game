f803a6edcaef8563ceca74631839221d64c88d6b
Shubham <40031408+Shubham-100@users.noreply.github.com>
2022-10-09 12:44:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,150 +1,183 @@
-// Initializes the drawing canvas
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
-
-// all the constants
-const FPS = 12;
-const tile = 30;
-const tileSize = canvas.width / tile; // 600/30 = 20
-
-// SCORE
-let score = 0;
-let gameOver = false;
-
-// SNAKE
-let sx = 0;
-let sy = 0;
-let delta = 20;
-let xspeed = 0;
-let yspeed = 0;
-let tail = 2;
-const snakeParts = [];
-
-let gameStarted = false;
-
-// FOOD
-let fx = tileSize * Math.floor(Math.random() * tile);
-let fy = tileSize * Math.floor(Math.random() * tile);
-
-class snakePart {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+const canvas=document.getElementById('game');
+const ctx=canvas.getContext('2d');
+//increase snake size 
+class snakePart{
+constructor(x, y){
+    this.x=x;
+    this.y=y;
 }
 
-// driver function, start of the program
-main();
-
-// driver function, start of the program
-function main() {
-  fillBackground("black");
-  if (!gameOver) {
-    changeCoordinates();
-    drawScore();
-    drawFood("red");
-    drawSnake("green");
-    detectCollision();
-    detectGameOver();
-    setTimeout(main, 1000 / FPS);
-  } else {
-    ctx.fillStyle = "white";
-    ctx.font = "20px verdana";
-    ctx.fillText("GAME OVER!!!", canvas.width / 2 - 50, canvas.width / 2);
-    ctx.fillText(
-      "Your score: " + score,
-      canvas.width / 2 - 50,
-      canvas.width / 2 + 20
-    );
-  }
 }
 
-// add event listener
-document.addEventListener("keydown", keyDown);
+let speed=7;
+let tileCount=20; 
 
-function detectGameOver() {
-  if (sx < 0 || sx > canvas.width || sy < 0 || sy > canvas.height) {
-    gameOver = true;
-  }
+let tileSize=canvas.clientWidth/tileCount-2;
+let headX=10;
+let headY=10;
 
-  if (gameStarted) {
-    for (let i = 0; i < snakeParts.length; i++) {
-      let part = snakeParts[i];
-      if (part.x == sx && part.y == sy) {
-        gameOver = true;
-        break;
-      }
+// array for snake parts
+const snakeParts=[];
+let tailLength=2;
+
+//initialize the speed of snake
+let xvelocity=0;
+let yvelocity=0;
+
+//draw apple
+let appleX=5;
+let appleY=5;
+
+//scores
+let score=0;
+
+// create game loop-to continously update screen
+function drawGame(){
+    changeSnakePosition();
+    // game over logic
+    let result=isGameOver();
+    if(result){// if result is true
+        return;
     }
-  }
+    clearScreen();
+    drawSnake();
+    drawApple();
+  
+    checkCollision()
+    drawScore();
+    setTimeout(drawGame, 1000/speed);//update screen 7 times a second
+}
+//Game Over function
+function isGameOver(){
+    let gameOver=false; 
+    //check whether game has started
+    if(yvelocity===0 && xvelocity===0){
+        return false;
+    }
+    if(headX<0){//if snake hits left wall
+        gameOver=true;
+    }
+    else if(headX===tileCount){//if snake hits right wall
+        gameOver=true;
+    }
+    else if(headY<0){//if snake hits wall at the top
+        gameOver=true;
+    }
+    else if(headY===tileCount){//if snake hits wall at the bottom
+        gameOver=true;
+    }
+
+    //stop game when snake crush to its own body
+
+     for(let i=0; i<snakeParts.length;i++){
+         let part=snakeParts[i];
+         if(part.x===headX && part.y===headY){//check whether any part of snake is occupying the same space
+             gameOver=true;
+             break; // to break out of for loop
+         }
+     }
+    
+
+    //display text Game Over
+    if(gameOver){
+     ctx.fillStyle="white";
+     ctx.font="50px verdana";
+     ctx.fillText("Game Over! ", canvas.clientWidth/6.5, canvas.clientHeight/2);//position our text in center
+    }
+
+    return gameOver;// this will stop execution of drawgame method
 }
 
-function keyDown(event) {
-  if (event.keyCode == 37) {
-    if (xspeed == 1) return;
-    // LEFT
-    xspeed = -delta;
-    yspeed = 0;
-  } else if (event.keyCode == 38 && yspeed != 1) {
-    // UP
-    xspeed = 0;
-    yspeed = -delta;
-  } else if (event.keyCode == 39 && xspeed != -1) {
-    // RIGHT
-    xspeed = delta;
-    yspeed = 0;
-  } else if (event.keyCode == 40 && yspeed != -1) {
-    // DOWN
-    xspeed = 0;
-    yspeed = delta;
-  } else if (event.keyCode == 32 && gameOver == true) {
-    gameOver = false;
-  }
+// score function
+function drawScore(){
+ctx.fillStyle="white"// set our text color to white
+ctx.font="10px verdena"//set font size to 10px of font family verdena
+ctx.fillText("Score: " +score, canvas.clientWidth-50,10);// position our score at right hand corner 
+
 }
 
-function changeCoordinates() {
-  sx += xspeed;
-  sy += yspeed;
+// clear our screen
+ function clearScreen(){
+
+ctx.fillStyle= 'black'// make screen black
+ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)// black color start from 0px left, right to canvas width and canvas height
+
+ }
+ function drawSnake(){
+    
+    ctx.fillStyle="green";
+    //loop through our snakeparts array
+    for(let i=0;i<snakeParts.length;i++){
+        //draw snake parts
+        let part=snakeParts[i]
+         ctx.fillRect(part.x *tileCount, part.y *tileCount, tileSize,tileSize)
+    }
+    //add parts to snake --through push
+    snakeParts.push(new snakePart(headX,headY));//put item at the end of list next to the head
+    if(snakeParts.length>tailLength){
+        snakeParts.shift();//remove furthest item from  snake part if we have more than our tail size
+
+    }
+    ctx.fillStyle="orange";
+    ctx.fillRect(headX* tileCount,headY* tileCount, tileSize,tileSize)
+
+
+ }
+ function changeSnakePosition(){
+     headX=headX + xvelocity;
+     headY=headY+ yvelocity;
+     
+ }
+ function drawApple(){
+     ctx.fillStyle="red";
+     ctx.fillRect(appleX*tileCount, appleY*tileCount, tileSize, tileSize)
+ }
+ // check for collision and change apple position
+ function checkCollision(){
+     if(appleX==headX && appleY==headY){
+         appleX=Math.floor(Math.random()*tileCount);
+         appleY=Math.floor(Math.random()*tileCount);
+         tailLength++;
+         score++; //increase our score value
+
+     }
+ }
+ //add event listener to our body
+ document.body.addEventListener('keydown', keyDown);
+
+function keyDown()
+//up
+{
+    if(event.keyCode==38){
+        //prevent snake from moving in opposite direcction
+        if(yvelocity==1)
+        return;
+        yvelocity=-1;
+        xvelocity=0;
+        
+    }
+    //down
+    if(event.keyCode==40){
+        if(yvelocity==-1)
+        return;
+        yvelocity=1;
+        xvelocity=0;
+    }
+
+//left
+    if(event.keyCode==37){
+        if(xvelocity==1)
+        return;
+        yvelocity=0;
+        xvelocity=-1;
+    }
+    //right
+    if(event.keyCode==48){
+        if(xvelocity==-1)
+        return;
+        yvelocity=0;
+        xvelocity=1;
+    }
 }
 
-function fillBackground(color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function drawSnake(color) {
-  ctx.fillStyle = color;
-
-  for (let i = 0; i < snakeParts.length; i++) {
-    let part = snakeParts[i];
-    ctx.fillRect(part.x, part.y, tileSize, tileSize);
-  }
-
-  snakeParts.push(new snakePart(sx, sy));
-  if (snakeParts.length > tail) {
-    snakeParts.shift();
-  }
-
-  // head
-  ctx.fillStyle = "orange";
-  ctx.fillRect(sx, sy, tileSize, tileSize);
-}
-
-function drawFood(color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(fx, fy, tileSize, tileSize);
-}
-
-function detectCollision() {
-  if (fx == sx && fy == sy) {
-    fx = tileSize * Math.floor(Math.random() * tile);
-    fy = tileSize * Math.floor(Math.random() * tile);
-    score++;
-    tail++;
-  }
-}
-
-function drawScore() {
-  ctx.fillStyle = "white";
-  ctx.font = "20px verdana";
-  ctx.fillText(score, canvas.width - 25, 25);
-}
+ drawGame(); 
